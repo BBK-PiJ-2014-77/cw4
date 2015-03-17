e99e223c52fe9ce24adfdf2260bc5359864d8822
Ambrose <a.cole@mail.cryst.bbk.ac.uk>
2015-03-17 11:09:08 +0000

    Changed how Id's a recorded, put in a static linked list.  This will be interrogated when looking for a new Id.  Seems to work and hasn't broken the Contact test

--- src/Implementations/ContactImpl.java
+++ src/Implementations/ContactImpl.java
@@ -2,6 +2,8 @@ package Implementations;
 
 import cw4.Contact;
 
+import java.util.LinkedList;
+
 /**
  * Created by digibrose on 25/02/2015.
  */
@@ -12,13 +14,30 @@ public class ContactImpl implements Contact {
     private String name;
     private String notes;
 
+    static LinkedList<Integer> IdList = new LinkedList<Integer>();
+
 
     public ContactImpl( String name, String notes){
-        this.Id = NextId;
-        NextId = NextId + 1;
+     //   this.Id = NextId;
+     //   NextId = NextId + 1;
         this.name = name;
         this.notes = notes;
-    }
+        System.out.println("here list size is " + IdList.size());
+        int j = 0;
+        for (int i = 0; i < IdList.size(); i++) {
+            if (i != IdList.get(i)) {
+                this.Id = i;
+                IdList.add(i, i);
+                break;
+            }
+            j=i+1;
+        }
+            if (j == IdList.size()){
+                this.Id = j;
+                IdList.add(j);
+            }
+        }
+
 
     public ContactImpl(int Id, String name, String notes){
 
